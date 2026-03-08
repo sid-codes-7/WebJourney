fb49086fe47e123ffe32c32f5b3aa15e24a288f8
Sid A. <120816320+sid-codes-7@users.noreply.github.com>
2026-03-08 15:01:54 -0400

    Added randomly positioned stars based on window size + width

--- script.js
+++ script.js
@@ -5,12 +5,33 @@ const ctx = canvas.getContext("2d");//"getting the pencil to start drawing"
 canvas.height = window.innerHeight; // get the height of the window
 canvas.width = window.innerWidth;// get the width of the window
 
-ctx.fillStyle = "white"; // the next thing drawing should be color: white
-ctx.beginPath();//just "lifting" the pencil to start drawing
+for(let i = 0; i< 300; i++) {
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
 
-// (x, y , radius, start angle, full circle)   
-ctx.arc(200, 200, 2, 0, Math.PI * 2); // draw circle
+    ctx.fillStyle = "white"; // the next thing drawing should be color: white
+    ctx.beginPath();//just "lifting" the pencil to start drawing
 
-ctx.fill(); // render the shape
+    // (x, y , radius, start angle, full circle)   
+    ctx.arc(x, y, 2, 0, Math.PI * 2);
+
+    ctx.fill(); // render the shape
+
+}
+
+let centerX = canvas.width / 2;
+let centerY = canvas.height / 2;
+
+
+ctx.fillStyle = "white";
+ctx.beginPath();
+ctx.arc(centerX, centerY, 100, 0, Math.PI * 2);
+ctx.fill();
+
+ctx.fillStyle = "black";
+ctx.beginPath();
+ctx.arc(centerX, centerY, 80, 0, Math.PI * 2);
+
+ctx.fill();
 
 
